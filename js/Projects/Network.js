d9a6839393fdb8a187ad9eb8fe2e80f170b4b89e
Timbre Freeman <19438918+tztz8@users.noreply.github.com>
2018-02-07 09:00:36 -0800

    update server locashen

--- js/Projects/Network.js
+++ js/Projects/Network.js
@@ -52,7 +52,7 @@ function setup() {
     myCanvas.parent('myContainer'); // I am tell the p5 scrip to put the canvas where i have the id of myContainer
     background(bacColer);
 
-    socket = io.connect('https://10.0.0.215:8192');
+    socket = io.connect('https://timbre.0ssh.ga:8192');
     socket.on('mouse', updatDrawingFS);
     socket.on('users', updateOtherUser);
     socket.on('Tusersb', function (data) {
