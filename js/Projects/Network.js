4604fc93d6b0e893ee2d8e710dbc10144f2121ad
Timbre Freeman <19438918+tztz8@users.noreply.github.com>
2018-09-07 13:56:37 -0700

    timbre.0ssh.ga --> tftinker.club

--- js/Projects/Network.js
+++ js/Projects/Network.js
@@ -52,7 +52,7 @@ function setup() {
     myCanvas.parent('myContainer'); // I am tell the p5 scrip to put the canvas where i have the id of myContainer
     background(bacColer);
 
-    socket = io.connect('https://timbre.0ssh.ga:8192');
+    socket = io.connect('https://home.tftinker.club:8192');
     socket.on('mouse', updatDrawingFS);
     socket.on('users', updateOtherUser);
     socket.on('Tusersb', function (data) {
